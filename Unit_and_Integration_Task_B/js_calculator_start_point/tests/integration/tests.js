949ff184e248947a899fec1e5c41553f77c3a635
Daniel Stewart <mgdog1@googlemail.com>
2018-08-06 13:54:09 +0100

    finished integration tests

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -68,5 +68,67 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('72');
   })
 
+  it('can chain operations', function(){
+    running_total = element(by.css('#running_total'));
+    //addition
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_subtract')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10');
+
+    //subtraction
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3');
+
+    //division
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5');
+
+    //multiplication
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6');
+  })
+
+  it('can handle large numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    for(i = 0; i < 20; i++){
+      element(by.css('#number0')).click();
+    }
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number1')).click();
+    for(i = 0; i < 20; i++){
+      element(by.css('#number0')).click();
+    }
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1e+40');
+  })
+
+  it('can handle negative numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-8');
+  })
+
+  it('can handle decimal numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.001');
+  })
+
+
 
 });
